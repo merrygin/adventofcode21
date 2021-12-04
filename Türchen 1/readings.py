caaa8391c4c254828e6da7e46b2b1ab56013bb9b
merrygin <smitzjak@hu-berlin.de>
2021-12-04 11:58:41 +0100

    Clean up Part 1

--- Türchen 1/readings.py
+++ Türchen 1/readings.py
@@ -1,26 +1,23 @@
 with open("Türchen 1/readings.txt", "r") as readings_file:
   readings_content = readings_file.readlines()
 
+# prepare the counting of larger readings
 larger_readings = 0
+index = 0
 
+# convert txt into list of ints
 readings_ints = []
-
 for line in readings_content:
     readings_ints.append(int(line))
 
-index = 0
-
+# take every reading and compare to the next by slicing it out of the whole list
 for reading in readings_ints:
+    # use indeces as slice parameters
     next_index = index + 1
     end_index = next_index +1
-    print(index, next_index, end_index)
     for reading2 in readings_ints[next_index:end_index]:
         if reading2 > reading:
             larger_readings += 1
         index += 1
 
-
-
-"""print(readings_ints)
-"""
 print(larger_readings)
