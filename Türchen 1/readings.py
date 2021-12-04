9c679baa5ac3663d613bab43254d7089c53d7da7
merrygin <smitzjak@hu-berlin.de>
2021-12-04 11:56:38 +0100

    Türchen 1 Part 1 geschafft

--- Türchen 1/readings.py
+++ Türchen 1/readings.py
@@ -8,16 +8,16 @@ readings_ints = []
 for line in readings_content:
     readings_ints.append(int(line))
 
+index = 0
 
-for reading in readings_ints[:14:1]:
-    # ok, das geht nicht, weil die werte von reading öfter vorkommen... so nimmt er nicht immer den korrekten index
-    next_index = readings_ints.index(reading) + 1
-    end_index = next_index + 1
-    print(readings_ints.index(reading))
+for reading in readings_ints:
+    next_index = index + 1
+    end_index = next_index +1
+    print(index, next_index, end_index)
     for reading2 in readings_ints[next_index:end_index]:
-        #print(reading2, readings_ints.index(reading2))
         if reading2 > reading:
             larger_readings += 1
+        index += 1
 
 
 
